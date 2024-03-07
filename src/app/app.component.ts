bef367d18a89510ea5e959e719db407ad021cee2
FranJRG <framirezgarcia0@gmail.com>
2024-03-07 14:22:28 +0100

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent,FooterComponent,NgxUiLoaderModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/interceptors/jwt.interceptor.ts
+++ src/app/shared/interceptors/jwt.interceptor.ts
@@ -1,6 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const loader = inject(NgxUiLoaderService);
+  loader.start();
   //Obtenemos el token del localStorage
   const token = localStorage.getItem('token');
   if(token){ //Si existe
@@ -8,5 +13,5 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
       setHeaders: {Authorization : token} //Lo añadimos a la cabecera
     })
   }
-  return next(req);
+  return next(req).pipe(finalize(() => loader.stop()));
 };
