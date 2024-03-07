4e329daedb54d692c5a6e210d3a7e4360f056269
FranJRG <framirezgarcia0@gmail.com>
2024-03-07 12:40:42 +0100

    carga

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './shared/interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes,withComponentInputBinding()), provideHttpClient()]
+  providers: [provideRouter(routes,withComponentInputBinding()), provideHttpClient(withInterceptors([jwtInterceptor]))]
 };
